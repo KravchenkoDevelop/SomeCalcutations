d107a31b42c5c02a8b1f791df9eec7ca88e45708
Tatiana Kravchenko <kravchenkodevelop@gmail.com>
2022-11-11 12:37:04 +0300

    test

---
+++ ConsoleApp1/DigitOperation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class DigitOperation
+    {
+        public static int[] GetRndArray(int length)
+        {
+            int[] array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = new Random().Next(100);
+            }
+
+            Array.Sort(array);
+
+            return array;
+        }
+
+        public static int[] ShiftArray(int[] arr)
+        {
+            int rnd = new Random().Next(10);
+            int[] array = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                array[i] = arr[(i + rnd) % arr.Length];
+            }
+
+            return array;
+        }
+
+        public static int MaxElemSearch(int[] arr)
+        {
+            return BinarySearch(arr);
+        }
+
+
+        private static int BinarySearch(int[] arr)
+        {
+            int result = 0;
+
+            if (arr[0] < arr[arr.Length / 2])
+            {
+
+            }
+            else if (arr[arr.Length - 1] < arr[arr.Length / 2])
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using ConsoleApp1;
+using System.ComponentModel.DataAnnotations;
+
+var rnd = new Random().Next(10);
+var arr = DigitOperation.GetRndArray(rnd);
+var shiftedArr = DigitOperation.ShiftArray(arr);
+Console.WriteLine($"{arr} {rnd}");
+Console.WriteLine($"{shiftedArr}");
